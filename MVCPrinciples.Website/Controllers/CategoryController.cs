c1d44f6718c66a2c3de5b90b0c36ffbb30066fb3
Aliaksandr Beliakou <Aliaksandr_Beliakou@epam.com>
2022-06-05 09:33:17 +0300

    Add products & categories empty pages.

---
+++ MVCPrinciples.Website/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCPrinciples.Website.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CategoryController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCPrinciples.Website/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCPrinciples.Website.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ProductController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
