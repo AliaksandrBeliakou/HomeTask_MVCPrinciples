2be4c29a3cbd21d3a17a9a6c7536114c5a28c662
Aliaksandr Beliakou <Aliaksandr_Beliakou@epam.com>
2022-06-05 15:10:02 +0300

    Task 2 - Configuration for products on page amount

--- MVCPrinciples.Website/Controllers/ProductController.cs
+++ MVCPrinciples.Website/Controllers/ProductController.cs
@@ -9,11 +9,13 @@ namespace MVCPrinciples.Website.Controllers
     {
         private readonly ILogger<HomeController> logger;
         private readonly NorthwindContext context;
+        private readonly int productOnPageAmount;
 
-        public ProductController(ILogger<HomeController> logger, NorthwindContext context)
+        public ProductController(NorthwindContext context, ILogger<HomeController> logger, IConfiguration configuratrion)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.context = context ?? throw new ArgumentNullException(nameof(context));
+            this.productOnPageAmount = configuratrion?.GetValue<int>("ProductsOnPageAmount") ?? throw new ArgumentNullException(nameof(configuratrion));
         }
 
         public IActionResult Index()
@@ -31,6 +33,11 @@ namespace MVCPrinciples.Website.Controllers
                 ReorderLevel = p.ReorderLevel,
                 Discontinued = p.Discontinued,
             });
+            if (productOnPageAmount > 0)
+            {
+                products = products.Take(productOnPageAmount);
+            }
+
             return View(products);
         }
     }
